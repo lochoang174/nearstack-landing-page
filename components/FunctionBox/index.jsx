dc7b6f0f0285fdc5f33cc320b3fd12354d58b810
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-10-31 22:05:41 +0700

    update demo link

--- components/FunctionBox/index.jsx
+++ components/FunctionBox/index.jsx
@@ -9,7 +9,7 @@ function FunctionBox(props) {
             <video autoPlay={true} loop preload="auto" playsInline muted={true} src={data.src || 'https://framerusercontent.com/assets/Sijo8uPEuwPBpBKSBNweaOSEj4.mp4'} className='w-[80%] h-full rounded-sm'></video>
             
             {data.button&&<div
-                onClick={() =>  window.location.href = 'https://nearstack.weminal.com/demo'}
+                onClick={() =>  window.location.href = 'http://13.212.244.67:3003/'}
             className="mt-6 bg-[var(--primary)] p-4 cursor-pointer hover:scale-[1.05] duration-100 ease-in transition-all hover:shadow-[var(--primary)] hover:shadow-sm rounded-[4px]">
                 <span className='text-white uppercase font-[700]' >Try it out</span>
                 </div>}</> );
