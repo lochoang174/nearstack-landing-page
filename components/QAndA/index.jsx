cfeb727acc258c0e7a56b323cadcec28f4f993ea
quigiaosu103 <52100996@student.tdtu.edu.vn>
2024-10-31 19:42:18 +0700

    fix answer show

--- components/QAndA/index.jsx
+++ components/QAndA/index.jsx
@@ -5,7 +5,7 @@ function QAndA(props) {
     return ( <div className='my-4'>
         <span onClick={() => {
             setShow(!show)}} className='text-[24px] font-[600] text-[#cacaca] block mb-4 cursor-pointer'>{data.question}</span>
-        {show && <span className={`text-[16px] h-[${show?24:0}px] transition-all duration-100 ease-in font-[400] text-[#cacaca] block`}>{data.awnser}</span>}
+        {show && <span className={`text-[16px]  transition-all duration-100 ease-in font-[400] text-[#cacaca]`}>{data.answer||'answer undefined'}</span>}
       </div> );
 }
 
